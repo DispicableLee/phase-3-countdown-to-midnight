b7d50c62cc3181f4d48da4c031342781831bf450
DispicableLee <lee.robert053@gmail.com>
2022-11-01 11:04:28 -0400

    done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(num)
+    while num > 1
+        num -= 1
+        puts "#{num} SECONDS!"
+    end
+    puts "HAPPY NEW YEAR!"
 end
+
+countdown(10)
+
+def countdown_with_sleep(num, sleep_num)
+    while num > 1
+        num -= 1
+        puts "#{num} SECONDS!"
+        sleep sleep_num
+    end
+    puts "HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep(10, 1)
